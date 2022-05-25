6dc7b2b6aee4b2eef8a80e2493a904b6f3226fdd
tomasDelizia <tomidelizia@gmail.com>
2022-05-24 22:12:32 -0300

    3 files modified

--- SimuladorColas.ts
+++ SimuladorColas.ts
@@ -430,14 +430,7 @@ export class SimuladorColas {
           tiempoPaseEntreControlYEmbarque = null;
           finPaseEntreControlYEmbarque = null;
           // Buscamos el pasajero que llegó a embarque y lo eliminamos del sistema.
-          
-
-          //delete pasajerosEnSistema[pasajerosEnSistema.findIndex(pasajero => pasajero.getEstado() === EstadoPasajero.PASANDO_DE_CONTROL_A_EMBARQUE)];
-          console.log("Antes de splice")
-          console.log(pasajerosEnSistema)
-          pasajerosEnSistema.splice(pasajerosEnSistema.findIndex(pasajero => pasajero.getEstado() === EstadoPasajero.PASANDO_DE_CONTROL_A_EMBARQUE));
-          console.log("después de splice")
-          console.log(pasajerosEnSistema)
+          pasajerosEnSistema.splice(pasajerosEnSistema.findIndex(pasajero => pasajero.getEstado() === EstadoPasajero.PASANDO_DE_CONTROL_A_EMBARQUE), 1);
           break;
         }
 
@@ -446,8 +439,6 @@ export class SimuladorColas {
           break;
         }
       }
-
-      
 
       evento.push(
         i,
